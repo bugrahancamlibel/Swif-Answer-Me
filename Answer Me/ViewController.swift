791f278abfd0bf55ac5f8bd3b8e24dc3134f465b
Buğrahan Çamlıbel <bugrahan.camlibel@oyakyatirim.com.tr>
2024-08-14 09:22:41 +0300

    Initial commit

--- Answer Me/ViewController.swift
+++ Answer Me/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var answerLabel: UILabel!
+    let answerList = ["Yes!", "No", "Maybe", "Absolutely Yes!", "Hell No!"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func learnButtonPressed(_ sender: UIButton) {
+        if let answer = answerList.randomElement() {
+            answerLabel.text = answer
+        } else {
+            answerLabel.text = "..."
+        }
+        
+    }
+    
 }
 
